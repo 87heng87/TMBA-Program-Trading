85e0f8dcc84f0bd96a2cc3f5e98ccc82975973e8
87heng87 <79631129+87heng87@users.noreply.github.com>
2021-09-10 09:57:20 +0800

    Update

--- TMBA_PTD.py
+++ TMBA_PTD.py
@@ -77,7 +77,7 @@ def Signal(data: pd.DataFrame):
 '''報酬率採用每日報酬率累積總和之方式計算'''
 def Returns(data: pd.DataFrame, signal: pd.DataFrame):
     discount = 0.6
-    fee = 1.425/1000 * discount 
+    fee = 1.425/1000 * discount
     ret_df = pd.concat([data['Close'].pct_change(), signal['all'], signal['long'], signal['short']], axis=1)
     ret_df.columns = ['d_ret', 'position_a', 'position_l', 'position_s']
     ret_df['return_a'] = ret_df['d_ret'] * ret_df['position_a']
@@ -141,7 +141,7 @@ class Ratio:
         return self.data.dropna().cumsum()[-1]
 
     def Annual_Return(self):
-        return (self.data.dropna().cumsum()[-1]+1)**(1/(len(self.data)/250))-1
+        return ((1 + self.data.mean()) ** 252)-1
 
     def Annual_Volatility(self):
         return self.data.std() * (250 ** 0.5)
@@ -152,12 +152,12 @@ class Ratio:
         return self.data.dropna().cumsum()[-1] / MDD
 
     def Sharpe(self):
-        rf = 0.000005
-        return (self.data.mean() - rf) / self.data.std()
+        rf = (0.002358 + 1) ** (1/252) - 1
+        return ((self.data.mean() - rf)/self.data.std()) * (252 ** 0.5)
 
     def Sortino(self):
-        rf = 0.000005
-        return (self.data.mean() - rf) / self.data[self.data < 0].std()
+        rf = (0.002358 + 1) ** (1/252) - 1
+        return ((self.data.mean() - rf)/self.data[self.data < 0].std()) * (252 ** 0.5)
 
 
 '''高原圖'''
@@ -169,19 +169,17 @@ class Optimization_Map:
         self.mode = str(mode)
 
     def CumRet(self, xfrom, xto, xby, yfrom, yto, yby):
-        for i in range(xfrom, xto, xby):
-            for j in range(yfrom, yto, yby):
-                I = i / 100
-                J = j / 100
-                temp_band = BBands(stock, self.mode, 20, I, J)
+        for i in np.arange(xfrom, xto, xby):
+            for j in np.arange(yfrom, yto, yby):
+                temp_band = BBands(stock, self.mode, 20, i, j)
                 temp_df = pd.concat([stock, temp_band], axis=1)
                 temp_signal = Signal(temp_df)
                 return_df = pd.concat([temp_df['Close'].pct_change(), temp_signal['all']], axis=1)
                 return_df.columns = ['d_return', 'position_all']
                 return_df['return_a'] = return_df['d_return'] * return_df['position_all']
                 cumret = Ratio(return_df['return_a']).Cummulative_Return()
-                self.upwide.append(I)
-                self.dnwide.append(J)
+                self.upwide.append(i)
+                self.dnwide.append(j)
                 self.z.append(cumret)
 
         fig = plt.figure(figsize=(10, 5))
@@ -193,19 +191,17 @@ class Optimization_Map:
         plt.show()
 
     def RonMDD(self, xfrom, xto, xby, yfrom, yto, yby):
-        for i in range(xfrom, xto, xby):
-            for j in range(yfrom, yto, yby):
-                I = i / 100
-                J = j / 100
-                temp_band = BBands(stock, self.mode, 20, I, J)
+        for i in np.arange(xfrom, xto, xby):
+            for j in np.arange(yfrom, yto, yby):
+                temp_band = BBands(stock, self.mode, 20, i, j)
                 temp_df = pd.concat([stock, temp_band], axis=1)
                 temp_signal = Signal(temp_df)
                 return_df = pd.concat([temp_df['Close'].pct_change(), temp_signal['all']], axis=1)
                 return_df.columns = ['d_return', 'position_all']
                 return_df['return_a'] = return_df['d_return'] * return_df['position_all']
                 temp_MDD = Ratio(return_df['return_a']).MDD_Return()
-                self.upwide.append(I)
-                self.dnwide.append(J)
+                self.upwide.append(i)
+                self.dnwide.append(j)
                 self.z.append(temp_MDD)
 
         fig = plt.figure(figsize=(10, 5))
@@ -217,19 +213,17 @@ class Optimization_Map:
         plt.show()
 
     def Sharpe_Ratio(self, xfrom, xto, xby, yfrom, yto, yby):
-        for i in range(xfrom, xto, xby):
-            for j in range(yfrom, yto, yby):
-                I = i / 100
-                J = j / 100
-                temp_band = BBands(stock, self.mode, 20, I, J)
+        for i in np.arange(xfrom, xto, xby):
+            for j in np.arange(yfrom, yto, yby):
+                temp_band = BBands(stock, self.mode, 20, i, j)
                 temp_df = pd.concat([stock, temp_band], axis=1)
                 temp_signal = Signal(temp_df)
                 return_df = pd.concat([temp_df['Close'].pct_change(), temp_signal['all']], axis=1)
                 return_df.columns = ['d_return', 'position_all']
                 return_df['return_a'] = return_df['d_return'] * return_df['position_all']
                 temp_sharpe = Ratio(return_df['return_a']).Sharpe()
-                self.upwide.append(I)
-                self.dnwide.append(J)
+                self.upwide.append(i)
+                self.dnwide.append(j)
                 self.z.append(temp_sharpe)
 
         fig = plt.figure(figsize=(10, 5))
@@ -241,19 +235,17 @@ class Optimization_Map:
         plt.show()
 
     def Sortino_Ratio(self, xfrom, xto, xby, yfrom, yto, yby):
-        for i in range(xfrom, xto, xby):
-            for j in range(yfrom, yto, yby):
-                I = i / 100
-                J = j / 100
-                temp_band = BBands(stock, self.mode, 20, I, J)
+        for i in np.arange(xfrom, xto, xby):
+            for j in np.arange(yfrom, yto, yby):
+                temp_band = BBands(stock, self.mode, 20, i, j)
                 temp_df = pd.concat([stock, temp_band], axis=1)
                 temp_signal = Signal(temp_df)
                 return_df = pd.concat([temp_df['Close'].pct_change(), temp_signal['all']], axis=1)
                 return_df.columns = ['d_return', 'position_all']
                 return_df['return_a'] = return_df['d_return'] * return_df['position_all']
                 temp_sortino = Ratio(return_df['return_a']).Sortino()
-                self.upwide.append(I)
-                self.dnwide.append(J)
+                self.upwide.append(i)
+                self.dnwide.append(j)
                 self.z.append(temp_sortino)
 
         fig = plt.figure(figsize=(10, 5))
@@ -279,19 +271,20 @@ def Monte_Carlo_Simulation(data: pd.DataFrame, n):
 if __name__ == '__main__':
     stock = YahooData('2609.TW', '2010-01-01', '2021-09-01')
     mode = 'o'
-    bands = BBands(stock, mode, 20, 2, 2)
+    bands = BBands(stock, mode, 30, 2, 2)
     mix_data = pd.concat([stock, bands], axis=1)
     sig = Signal(mix_data)
     return_data = Returns(mix_data, sig)
     print(Yearly_return(return_data))
     PnL(return_data)
-
+    print(return_data['return_a'].mean())
+    print(return_data['return_a'].std())
 
 
     print('=========================Strategy Ratio==================================')
     print('累積日報酬率：', Ratio(return_data['return_a']).Cummulative_Return()*100, '%')
     print('年化報酬率：', Ratio(return_data['return_a']).Annual_Return() * 100, '%')
-    print('年化波動率：', Ratio(return_data['return_a']).Annual_Volatility())
+    print('年化波動率：', Ratio(return_data['return_a']).Annual_Volatility() * 100, '%')
     print('最大風險回撤：', max(-MDD_Series(return_data['return_a']))*100, '%')
     print('風報比：', Ratio(return_data['return_a']).MDD_Return())
     print('夏普率：', Ratio(return_data['return_a']).Sharpe())
@@ -299,17 +292,17 @@ if __name__ == '__main__':
     print('=========================Benchmark Ratio==================================')
     print('累積日報酬率：', Ratio(return_data['d_ret']).Cummulative_Return() * 100, '%')
     print('年化報酬率：', Ratio(return_data['d_ret']).Annual_Return() * 100, '%')
-    print('年化波動率：', Ratio(return_data['d_ret']).Annual_Volatility())
+    print('年化波動率：', Ratio(return_data['d_ret']).Annual_Volatility() * 100, '%')
     print('最大風險回撤：', max(-MDD_Series(return_data['d_ret'])) * 100, '%')
     print('風報比：', Ratio(return_data['d_ret']).MDD_Return())
     print('夏普率：', Ratio(return_data['d_ret']).Sharpe())
     print('索丁諾比率：', Ratio(return_data['d_ret']).Sortino())
 
 
-    Optimization_Map(mode).CumRet(25, 300, 25, 25, 300, 25)
-    Optimization_Map(mode).RonMDD(25, 300, 25, 25, 300, 25)
-    Optimization_Map(mode).Sharpe_Ratio(25, 300, 25, 25, 300, 25)
-    Optimization_Map(mode).Sortino_Ratio(25, 300, 25, 25, 300, 25)
+    Optimization_Map(mode).CumRet(0.25, 3.25, 0.25, 0.25, 3.25, 0.25)
+    Optimization_Map(mode).RonMDD(0.25, 3.25, 0.25, 0.25, 3.25, 0.25)
+    Optimization_Map(mode).Sharpe_Ratio(0.25, 3, 0.25, 0.25, 3.25, 0.25)
+    Optimization_Map(mode).Sortino_Ratio(0.25, 3, 0.25, 0.25, 3.25, 0.25)
 
 
     Monte_Carlo_Simulation(return_data, 50)
